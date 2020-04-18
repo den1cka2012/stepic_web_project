57758f17a7aba72f57c9d80d1a445ee3c2d393a9
den1cka2012@gmail.com <den1cka2012@gmail.com>
2020-04-18 20:30:30 +0300

    content length is removed

--- hello.py
+++ hello.py
@@ -4,8 +4,7 @@ def app(environ, start_response):
     # body = "\n".join(environ.get('QUERY_STRING').split("&"))
     status = '200 OK'
     response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(body)))
+        ('Content-type', 'text/plain')
     ]
     start_response(status, response_headers)
     return body
