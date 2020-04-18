bfa1ebe01ef3280c0a49a15e20a346f8234160d5
den1cka2012@gmail.com <den1cka2012@gmail.com>
2020-04-18 20:26:56 +0300

    added bytes using utf-8

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 def app(environ, start_response):
     """Simplest possible application object"""
-    # body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    body = "\n".join(environ.get('QUERY_STRING').split("&"))
+    body = [bytes(i + '\n', 'utf-8') for i in environ['QUERY_STRING'].split('&')]
+    # body = "\n".join(environ.get('QUERY_STRING').split("&"))
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
         ('Content-Length', str(len(body)))
     ]
     start_response(status, response_headers)
-    return [bytes(body)]
+    return body
