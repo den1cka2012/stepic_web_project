1018dda25c133ce6d123175bdec738117359bea5
den1cka2012@gmail.com <den1cka2012@gmail.com>
2020-04-19 16:13:55 +0300

    added test view pt. 4

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+from qa.views import test
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', include('qa.urls')), 
-    url(r'^question/(\d+)/$', include('qa.urls')),
+    url(r'^', test),
 ]
 
