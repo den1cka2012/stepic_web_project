a61f1e313492aca86beba0394d646d57411dcb69
den1cka2012@gmail.com <den1cka2012@gmail.com>
2020-05-03 18:55:50 +0300

    Views pt. 7

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -17,14 +17,13 @@ from django.conf.urls import url
 from qa.views import test, login, signup, question, ask, popular, new, qa_list_all, qa_popular_all
 
 urlpatterns = [
-    url(r'^$', test, name='test'),
+    url(r'^$', qa_list_all, name='main'),
     url(r'^\?page=(?P<page>\d+)', qa_list_all, name='main'),
     url(r'^test/', test, name='test'),
     url(r'^login/', login, name='login'),
     url(r'^signup/', signup, name='signup'),
     url(r'^question/(?P<id>\d+)/', question, name='question'),
     url(r'^ask/', ask, name='ask'),
-    url(r'^popular/', popular, name='popular'),
     url(r'^popular/\?page=(?P<page>\d+)', qa_popular_all, name='popular'),
     url(r'^new/', new, name='new'),
 ]
