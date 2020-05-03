3acdf28d473812682f01b362e30c718eac27eac8
den1cka2012@gmail.com <den1cka2012@gmail.com>
2020-05-03 16:55:49 +0300

    Pagination pt. 1

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -14,16 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from qa.views import test, login, signup, question, ask, popular, new
+from qa.views import test, login, signup, question, ask, popular, new, qa_list_all, qa_popular_all
 
 urlpatterns = [
     url(r'^$', test, name='test'),
+    url(r'^\?page=(?P<page>\d+)', qa_list_all, name='main'),
     url(r'^test/', test, name='test'),
     url(r'^login/', login, name='login'),
     url(r'^signup/', signup, name='signup'),
     url(r'^question/(?P<id>\d+)/', question, name='question'),
     url(r'^ask/', ask, name='ask'),
     url(r'^popular/', popular, name='popular'),
+    url(r'^popular/\?page=(?P<page>\d+)', qa_popular_all, name='popular')
     url(r'^new/', new, name='new'),
 ]
 
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from qa.models import Question, Answer
+from django.core.paginator import Paginator, EmptyPage
 
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
@@ -21,3 +23,42 @@ def popular(request, *args, **kwargs):
 
 def new(request, *args, **kwargs):
     return HttpResponse('OK')
+
+
+def paginate(request, qs):
+    try:
+        limit = int(request.GET.get('limit', 10))
+    except ValueError:
+        limit = 10
+
+    if limit > 100:
+        limit = 10
+
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError:
+        raise Http404
+
+    paginator = Paginator(qs, limit)
+    try:
+        page = paginator.page(page)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    return page, paginator
+
+def qa_list_all(request):
+    qa = Question.objects.all()
+    qa = qa.order_by('-added_at')
+    page, paginator = paginate(request, qa)
+
+    return render(request, 'questions_list.html',
+                  {'user': request.user, 'questions': page.object_list, 'paginator': paginator, 'page': page, })
+
+
+def qa_popular_all(request):
+    qa = Question.objects.all()
+    qa = qa.order_by('-rating')
+    page, paginator = paginate(request, qa)
+
+    return render(request, 'questions_popular_list.html',
+                  {'user': request.user, 'questions': page.object_list, 'paginator': paginator, 'page': page, })
